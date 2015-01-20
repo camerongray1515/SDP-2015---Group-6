9a947e7b87e84d80b32f46cf32d75768723171b8
Mattias Appelgren <mattiasappel@gmail.com>
2015-01-20 14:47:14 +0000

    Main function now supports cross platform wasd

--- main.py
+++ main.py
@@ -1,38 +1,44 @@
-from multiprocessing import Process, Queue
-from msvcrt import getch
+#from multiprocessing import Process, Queue
+#from msvcrt import getch
 
-from robot_api import RobotAPI
+from Control.robot_api import RobotAPI
+from Utility.Getch import _Getch
 
 def getInput(q):
      while True:
         q.put(ord(getch()))
 
+
+
+
+getch = _Getch()
+
 if __name__ == "__main__":
     robot = RobotAPI()
-    queue = Queue()
-    process = Process(target = getInput, args = (queue,))
-    process.start()
+    #queue = Queue()
+    #process = Process(target = getInput, args = (queue,))
+    #process.start()
 
     #main loop
-    while True:
-        if not queue.empty():
-            input = queue.get()
-            #print input #uncomment this to print keycodes for debugging
+    run = True
+    while run:
+        input = ord(getch())
+        print input #uncomment this to print keycodes for debugging
             
-            if(input == 97): # A
-                robot.turn_left()
-            if(input == 115): # S
-                robot.go_backward()
-            if(input == 100): # D
-                robot.turn_right()
-            if(input == 119): # W
-                robot.go_forward()
-            if(input == 32): # Spacebar
-                robot.kick()
-            if(input == 27): # Esc
-                robot.stop()
-
-    process.join()
+        if(input == 97): # A
+            robot.turn_left()
+        elif(input == 115): # S
+            robot.go_backward()
+        elif(input == 100): # D
+            robot.turn_right()
+        elif(input == 119): # W
+            robot.go_forward()
+        elif(input == 32): # Spacebar
+            robot.kick()
+        elif(input == 27): # Esc
+            robot.stop()
+        elif(input == 113): # Q
+            run = False
 
 
 
