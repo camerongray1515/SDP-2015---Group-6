1e42828ea3a17ccb34040964b6d99312e6b99453
Parker Lamb <parkerlamb35@gmail.com>
2015-03-16 14:05:45 +0000

    Re-added the deleted calibrations file and reset the AlignPlan to the previous commit. The master branch now works out of the box again as of this commit. If anyone deletes the calibrations again I will personally slap them.

--- planning/AlignPlan.py
+++ planning/AlignPlan.py
@@ -1,7 +1,6 @@
 from Plan import Plan
 from Utility.CommandDict import CommandDict
 import math
-from consol import log
 import pdb
 
 class AlignPlan(Plan):
@@ -21,9 +20,8 @@ class AlignPlan(Plan):
         """
 
         if self.world.ball is not None:
-            is_aligned = self.isAligned(self.robot)
-            log("Is aligned", is_aligned)
-            return not self.world.pitch.is_within_bounds(self.robot, self.world.ball.x, self.world.ball.y) and (not is_aligned)
+            return not self.world.pitch.is_within_bounds(self.robot, self.world.ball.x, self.world.ball.y) and \
+                   (not self.isAligned(self.robot))
         return False
 
     def nextCommand(self):
@@ -31,7 +29,7 @@ class AlignPlan(Plan):
         current_angle = self.robot.angle
         current_x = self.robot.x
         # If close to the edge of zone, return to the centre
-        zone = self.world.pitch.zones[self.robot.zone][0]
+        zone = self.world.pitch.zones[self.robot.zone][0] 
         #zone is of form [(115.0, 290.0), (255.0, 290.0), (255.0, 0.0), (115.0, 0.0)]
         min_x = 10000
         max_x = 0
@@ -49,23 +47,17 @@ class AlignPlan(Plan):
                 return command
 
 
-        #rot_speed = current_angle * 30 + 30
-        #if current_angle > math.pi:
-
-
-
         # Turn either left or right, depending on which requires the least turning
         if current_angle < math.pi:
             if current_angle < math.pi/2:
-                return CommandDict(rot_speed, "Left", "None")
+                return CommandDict(57, "Left", "None")
             else:
-                return CommandDict(rot_speed, "Right", "None")
-        else:
+                return CommandDict(57, "Right", "None")
+        else: 
             if current_angle < 3 * math.pi /2:
-
-                return CommandDict(rot_speed, "Left", "None")
-            else:
-                return CommandDict(rot_speed, "Right", "None")
+                return CommandDict(57, "Left", "None")
+            else: 
+                return CommandDict(57, "Right", "None")
 
     def __str__(self):
         return "align plan"
